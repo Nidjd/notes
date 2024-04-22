3713ab52318fb6b36cb40c72a28c230dc67a098c
Mohammed Al-mahmoud <Mohammed Al-mahmoud>
2024-04-22 13:59:15 +0300

    add the main colors to the project

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0XFFD04848);
+const Color secondryColor = Color(0XFFF3B95F);
+const Color thirdColor = Color(0XFFFDE767);
+const Color fourthColor = Color(0XFF6895D2);
