b6a6a3d344a6fb02af2712a58383f701c108c9b3
Mohammed Al-mahmoud <Mohammed Al-mahmoud>
2024-04-26 21:21:05 +0300

    add the sign out button , and it is working

--- lib/features/home/presentaion/views/home_page.dart
+++ lib/features/home/presentaion/views/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:notes/core/utils/router_app.dart';
 import 'package:notes/features/home/presentaion/views/manager/add_note_cubit/add_note_cubit.dart';
 
 import 'package:notes/features/home/presentaion/views/widgets/custom_floating_action_button.dart';
@@ -11,7 +14,19 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+                GoRouter.of(context).pushReplacement(AppRouter.kSignInRoute);
+              },
+              icon: const Icon(
+                Icons.login_outlined,
+              ))
+        ],
+      ),
       floatingActionButton: BlocProvider(
         create: (context) => AddNoteCubit(),
         child: const CustomFloatingActionButton(),
